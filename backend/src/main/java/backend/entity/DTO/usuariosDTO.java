ea4eaeafb09506e0d925938c4b5938641143337c
D M I <ademirjuniorfertonani@gmail.com>
2025-01-28 14:21:12 -0300

    DTO

--- backend/src/main/java/backend/entity/DTO/usuariosDTO.java
+++ backend/src/main/java/backend/entity/DTO/usuariosDTO.java
@@ -1,18 +1,40 @@
 package backend.entity.DTO;
 
 import backend._config.Enums.Status;
-import lombok.Data;
 
 import java.io.Serial;
 
-@Data
+
 public class usuariosDTO {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    private Long id;
+
     private String name;
     private Status status;
 
+    public usuariosDTO() {
+    }
+
+    public usuariosDTO(String name, Status status) {
+        this.name = name;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 
 }
