ae6bb5a6e9e35f51a4b37b144f99652deb58408a
D M I <ademirjuniorfertonani@gmail.com>
2025-01-28 13:07:52 -0300

    Usuarios

--- backend/src/main/java/backend/entity/model/Usuarios.java
+++ backend/src/main/java/backend/entity/model/Usuarios.java
@@ -1,6 +1,6 @@
-package Back.videos.entity.model;
+package backend.entity.model;
 
-import Back._config.Enums.Genres;
+import backend._config.Enums.Status;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,71 +11,14 @@ import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
-public class Video implements Serializable {
+public class Usuarios implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idVideo;
-
+    private Long id;
     private String name;
-    private Genres genres;
-    private String description;
+    private Status status;
 
-
-    public Video() {
-    }
-
-    public Video(Long idVideo, String name, Genres genres, String description) {
-        this.idVideo = idVideo;
-        this.name = name;
-        this.genres = genres;
-        this.description = description;
-    }
-
-    public Long getIdVideo() {
-        return idVideo;
-    }
-
-    public void setIdVideo(Long idVideo) {
-        this.idVideo = idVideo;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Genres getGenres() {
-        return genres;
-    }
-
-    public void setGenres(Genres genres) {
-        this.genres = genres;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Video video = (Video) o;
-        return Objects.equals(idVideo, video.idVideo);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(idVideo);
-    }
 }
