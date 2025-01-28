461d59c1521a0713cc493ee80247dbf3f4f7bd45
D M I <ademirjuniorfertonani@gmail.com>
2025-01-28 13:12:09 -0300

    usuariosRepository

--- backend/src/main/java/backend/repository/usuariosRepository.java
+++ backend/src/main/java/backend/repository/usuariosRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface usuarioRepository extends JpaRepository<Usuarios, Long> {
+public interface usuariosRepository extends JpaRepository<Usuarios, Long> {
 }
---
+++ backend/src/main/java/backend/resource/usuariosResource.java
@@ -0,0 +1,2 @@
+package backend.resource;public class usuariosResource {
+}
