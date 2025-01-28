9c1d62769f9bd18acb7e0ec6b5059f0af0139d69
D M I <ademirjuniorfertonani@gmail.com>
2025-01-28 14:45:30 -0300

    upDate usuarioDTO

--- backend/src/main/java/backend/entity/DTO/usuariosDTO.java
+++ backend/src/main/java/backend/entity/DTO/usuariosDTO.java
@@ -9,16 +9,25 @@ public class usuariosDTO {
     @Serial
     private static final long serialVersionUID = 1L;
 
-
+    private Long id;
     private String name;
     private Status status;
 
     public usuariosDTO() {
     }
 
-    public usuariosDTO(String name, Status status) {
+    public usuariosDTO(Long id, String name, Status status) {
+        this.id = id;
         this.name = name;
         this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
