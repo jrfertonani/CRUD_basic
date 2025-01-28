9006e27a906df975803cd0931f917e591c93a84c
D M I <ademirjuniorfertonani@gmail.com>
2025-01-28 13:13:03 -0300

    usuariosService

--- backend/src/main/java/backend/service/usuariosService.java
+++ backend/src/main/java/backend/service/usuariosService.java
@@ -1,2 +1,18 @@
-package backend.service;public class usuariosService {
+package backend.service;
+
+import backend.repository.usuariosRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class usuariosService {
+
+    @Autowired
+    public ModelMapper mapper;
+
+    @Autowired
+    public usuariosRepository repository;
+
+    
 }
