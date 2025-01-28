cb6275205477a5648b8eb351bc27794af59f4878
D M I <ademirjuniorfertonani@gmail.com>
2025-01-28 13:12:38 -0300

    usuariosresource

--- backend/src/main/java/backend/resource/usuariosResource.java
+++ backend/src/main/java/backend/resource/usuariosResource.java
@@ -1,2 +1,21 @@
-package backend.resource;public class usuariosResource {
+package backend.resource;
+
+import backend.service.usuariosService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/usuarios")
+public class usuariosResource {
+
+    @Autowired
+    public ModelMapper mapper;
+
+    @Autowired
+    public usuariosService service;
+
 }
